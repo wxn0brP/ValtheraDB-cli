e0a9b4f13a623ca0be855ef894588a2a2d988903
wxn0brP <toja.filiper@gmail.com>
2025-08-02 19:16:15 +0200

    feat: update server

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,17 @@ import { Valthera } from "@wxn0brp/db";
 import deserializeFunctions from "./function";
 import path from "path";
 import FalconFrame from "@wxn0brp/falcon-frame";
+import { createCORSPlugin } from "@wxn0brp/falcon-frame/plugins/cors";
+import JSON5 from "json5";
 
 const app = new FalconFrame();
 const dbDir = process.env.DB_DIR || process.cwd();
 const port = parseInt(process.env.PORT) || 3333;
 const db = new Valthera(dbDir);
 
+app.use(createCORSPlugin(["*"]).process);
+
 app.post("/db/:type", async (req, res) => {
-    req.headers["access-control-allow-origin"] = "*";
     const { type } = req.params;
     const { params, keys } = req.body as {
         params: unknown[],
@@ -25,6 +28,10 @@ app.post("/db/:type", async (req, res) => {
         res.status(400);
         return { err: true, msg: "params is required" };
     }
+
+    const paramArgs = params.map(param => JSON5.stringify(param));
+    const str = type + "(" + paramArgs.join(", ") + ")";
+    console.log(str);
 
     try {
         const parsedParams = deserializeFunctions(params, keys || []) as any;
